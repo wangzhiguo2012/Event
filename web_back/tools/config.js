7f486bdb30696b0b727d6e9c45c52a3c81465463
王志国 <1762453992@qq.com>
2019-12-15 17:29:13 +0800

    test

---
+++ web_back/tools/config.js
@@ -0,0 +1,16 @@
+
+// 这个js文件中用来保存网站中的一些配置信息
+// 例如所有的接口地址，都可以保存在这个文件中
+// 下面这些保存的配置信息的变量最好避免被其他代码不小心修改了
+// 可以变量命名书写为全大写的形式
+// 命名的多个部分使用——连接（下划线命名方法）
+
+
+
+
+var BASE_URL = 'http://localhost:8000';
+
+
+
+var LOGIN = BASE_URL + '/admin/login';
+var LOGOUT = BASE_URL + '/admin/logout'
---
+++ web_back/tools/user.js
@@ -0,0 +1,40 @@
+// 使用user.jsdui 对项目中域用户操作有关的请求功能进行保存
+
+var user = {
+    // 设置login方法，用来设置方法
+    login: function (options) {
+        $.ajax({
+            type: 'post',
+            url: LOGIN,
+            data: {
+                // 第一个 user_name是data的属性
+                // 第二个是 user_name是html中data的实参
+                user_name: options.data.user_name,
+                password: options.data.password
+            },
+            success: function (res) {
+                if (res.code === 200) {
+                    options.success();
+                } else {
+                    options.fail();
+                }
+
+            }
+        });
+    },
+    logout: function (options) {
+        $.ajax({
+            type: 'post',
+            url: LOGOUT,
+
+            success: function (res) {
+                if (res.code === 200) {
+                    options.success();
+                } else {
+                    options.fail();
+                }
+
+            }
+        });
+    }
+};
